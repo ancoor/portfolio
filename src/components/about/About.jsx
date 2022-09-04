959bd49dd2b1989ad4a27ce20a9b9be6966f77e6
ancoor <ancoorbanerjee7@gmail.com>
2022-09-04 16:33:58 +0530

    formating

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,79 +1,75 @@
-import React,{useEffect, useState} from 'react';
-import SkillCard from './SkillCard';
+import React, { useEffect, useState } from "react";
+import SkillCard from "./SkillCard";
 // import {skills} from '../data/about_data'
-import {motion} from 'framer-motion'
-import './about.css'
-import { useStateContext } from '../../contexts/ContextProvider';
+import { motion } from "framer-motion";
+import "./about.css";
+import { useStateContext } from "../../contexts/ContextProvider";
 const about_variant = {
-    hidden: {
-        opacity:0
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.2,
+      duration: 0.5,
     },
-    visible: {
-        opacity: 1,
-        transition: {
-            delay: 0.2,
-            duration: 0.5,
-        }
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      ease: "easeInOut",
     },
-    exit: {
-        opacity: 0,
-        transition: {
-            ease: 'easeInOut'
-        }
-    }
-}
+  },
+};
 
 const About = () => {
-    const [skillData,setSkillData] = useState([]);
-    const [skillHeader,setSkillHeader] = useState("");
-    const {  showLoader, hideLoader } = useStateContext();
-    const apiSkills = async() => {
-        showLoader();
-         await fetch('https://portfolio-user-service.herokuapp.com/api/portfolio/about?user=1')
-        .then(response => response.json())
-        .then(({data}) => {
-            setSkillData(data)
-            // console.log('api',data.result[0])
-            });
-         await fetch('https://portfolio-user-service.herokuapp.com/api/portfolio/about-header?user=1')
-        .then(response => response.json())
-        .then(({data}) => {
-            setSkillHeader(data.header)
-            hideLoader();
-            // console.log('api',data.result[0])
-            });
-        
+  const [skillData, setSkillData] = useState([]);
+  const [skillHeader, setSkillHeader] = useState("");
+  const { showLoader, hideLoader } = useStateContext();
+  const apiSkills = async () => {
+    showLoader();
+    await fetch(
+      "https://portfolio-user-service.herokuapp.com/api/portfolio/about?user=1"
+    )
+      .then((response) => response.json())
+      .then(({ data }) => {
+        setSkillData(data);
+      });
+    await fetch(
+      "https://portfolio-user-service.herokuapp.com/api/portfolio/about-header?user=1"
+    )
+      .then((response) => response.json())
+      .then(({ data }) => {
+        setSkillHeader(data.header);
+        hideLoader();
+      });
+  };
+  useEffect(() => {
+    if (skillData.length === 0) {
+      apiSkills();
     }
-    useEffect(() => {
-        if(skillData.length === 0)
-        {
-            apiSkills();
-        }
-        console.log('languages',skillData)
-        }, [skillData]);
-    return (
-        <motion.div className="about"
-            variants={about_variant}
-            initial='hidden'
-            animate='visible'
-            exit="exit"
-        >
-            <h6 className="about__intro">
-                {skillHeader}
-            </h6>
-            <div className="container about__container">
-                <h6 className="about__heading">What I offer</h6>
-                <div className="row">
-                    {
-                        skillData.map(skill =>
-                            <SkillCard key={skill.title} skill={skill} />
-
-                        )
-                    }
-                </div>
-            </div>
-        </motion.div>
-    );
+    // eslint-disable-next-line
+  }, [skillData]);
+  return (
+    <motion.div
+      className="about"
+      variants={about_variant}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      <h6 className="about__intro">{skillHeader}</h6>
+      <div className="container about__container">
+        <h6 className="about__heading">What I offer</h6>
+        <div className="row">
+          {skillData.map((skill) => (
+            <SkillCard key={skill.title} skill={skill} />
+          ))}
+        </div>
+      </div>
+    </motion.div>
+  );
 };
 
 export default About;
