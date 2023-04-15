a7d7f002f3189d86eb0080d3736bdd86f5e7f005
Ancoor Banerjee <ancoorbanerjee7@gmail.com>
2023-04-15 11:46:19 +0530

    render to vercel

--- src/components/data/config.js
+++ src/components/data/config.js
@@ -1,37 +1,37 @@
 export const service = {
     portfolio: {
         about: {
-            url: 'https://portfolio-service-dv30.onrender.com/api/portfolio/about',
+            url: 'https://portfolio-dashboard-service.vercel.app/api/portfolio/about',
             dev: 'http://localhost:8080/api/portfolio/about'
         },
         contact: {
-            url: 'https://portfolio-service-dv30.onrender.com/api/portfolio/contact',
+            url: 'https://portfolio-dashboard-service.vercel.app/api/portfolio/contact',
             dev: 'http://localhost:8080/api/portfolio/contact'
         },
         resume: {
             experience: {
-                url: 'https://portfolio-service-dv30.onrender.com/api/portfolio/resume/experience',
+                url: 'https://portfolio-dashboard-service.vercel.app/api/portfolio/resume/experience',
                 dev: 'http://localhost:8080/api/portfolio/resume/experience'
             },
             skillset: {
-                url: 'https://portfolio-service-dv30.onrender.com/api/portfolio/resume/skillset',
+                url: 'https://portfolio-dashboard-service.vercel.app/api/portfolio/resume/skillset',
                 dev: 'http://localhost:8080/api/portfolio/resume/skillset'
             },
             resume: {
-                url: 'https://portfolio-service-dv30.onrender.com/api/portfolio/resume',
+                url: 'https://portfolio-dashboard-service.vercel.app/api/portfolio/resume',
                 dev: 'http://localhost:8080/api/portfolio/resume'
             }
         },
         upload: {
-            url: 'https://portfolio-service-dv30.onrender.com/api/portfolio/upload',
+            url: 'https://portfolio-dashboard-service.vercel.app/api/portfolio/upload',
             dev: 'http://localhost:8080/api/portfolio/upload'
         },
         files: {
-            url: 'https://portfolio-service-dv30.onrender.com/uploads',
+            url: 'https://portfolio-dashboard-service.vercel.app/uploads',
             dev: 'http://localhost:8080/uploads'
         },
         project: {
-            url: 'https://portfolio-service-dv30.onrender.com/api/portfolio/projects',
+            url: 'https://portfolio-dashboard-service.vercel.app/api/portfolio/projects',
             dev: 'http://localhost:8080/api/portfolio/projects'
         },
     },
